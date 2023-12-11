f7a2f5741838c9d00e178932e4cd2f11dcea05d9
oneVsix9 <audsonev69@gmail.com>
2023-12-11 22:42:49 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+include 'connect.php';
+session_start();
+
+if (isset($_POST['inMail']) && isset($_POST['inPass'])) {
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $mail = validate($_POST['inMail']);
+    $pass = validate($_POST['inPass']);
+    if (isset($_POST['signIn'])) {
+
+
+        $email = $_POST['inMail'];
+        $password = $_POST['inPass'];
+
+        if ($email == 'admin@gmail.com' && $password == 'admin') {
+            echo "<script>window.location.href ='admin-homepage.html'</script>";
+            // header('admin-homepage.html');
+            exit();
+        } 
+        elseif ($email != NULL && $password != NULL) {
+            $sql = "SELECT * from user_t where email='$email' and password='$password'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($result);
+            if (mysqli_num_rows($result) === 1) {
+                if ($row['email'] === $email && $row['password'] === $password) {
+                    $_SESSION['email'] = $email;
+
+                    echo "<script>window.location.href ='index.html'</script>";
+                    //header('');
+                    exit();
+                }
+            }
+        }
+    }
+}
