738e6a24f88d14beb49cd1d49754e141e9d78e42
Sourav <samiul.sourav00@gmail.com>
2023-12-13 23:08:27 +0600

    favicon added

--- contact.php
+++ contact.php
@@ -11,6 +11,8 @@ session_start();
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+	<link rel="icon" type="image/x-icon" href="/images/leaf.png">
+
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
